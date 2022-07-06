272e596ba4d13b50cc16bb021e4432f17e399b2f
syahlaa14 <108814165+syahlaa14@users.noreply.github.com>
2022-07-06 20:51:47 +0700

    Add files via upload

---
+++ TUBES CHAPTER 2 SELESEEE/order.js
@@ -0,0 +1,42 @@
+const Orders = [
+    {
+        Pemesan: 'Zulfan Hanif', 
+        Kamar: 'Deluxe 04',
+        NomorHp: '08888888',
+        CheckIn: '7/5/2022',
+        CheckOut: '7/10/2022',
+        JumlahKamar: '1'
+    },
+    {
+        Pemesan: 'Hans Christian', 
+        Kamar: 'Superior 10',
+        NomorHp: '08000000',
+        CheckIn: '7/4/2022',
+        CheckOut: '7/6/2022',
+        JumlahKamar: '1'
+    },
+    {
+        Pemesan: 'Syahla Meisyha', 
+        Kamar: 'Deluxe 01',
+        NomorHp: '0855555555',
+        CheckIn: '7/1/2022',
+        CheckOut: '7/15/2022',
+        JumlahKamar: '1'
+    },
+    {
+        Pemesan: 'Muhammad Zidane', 
+        Kamar: 'Deluxe 15',
+        NomorHp: '08999999999',
+        CheckIn: '7/6/2022',
+        CheckOut: '7/9/2022',
+        JumlahKamar: '1'
+    },
+    {
+        Pemesan: 'Shellyn Euriska', 
+        Kamar: 'Superior 08',
+        NomorHp: '0811111111',
+        CheckIn: '7/2/2022',
+        CheckOut: '7/8/2022',
+        JumlahKamar: '1'
+    },
+    ]
